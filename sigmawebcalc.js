bf605a6328315831142ad8c230b9df1de86e1313
rroque6428 <rroque6428@terra.com.br>
2015-09-14 18:18:19 -0300

    Adição de Novos Comentários

--- sigmawebcalc.js
+++ sigmawebcalc.js
@@ -1,5 +1,5 @@
 /*
-   SIGMAWEBCALC v0.1 (Open Source Project)
+   SIGMAWEBCALC v0.0 (Open Source Project)
    -----------------
    O codigo abaixo e' responsavel por dar funcionalidade a Calculadora.
    Tentamos comentar o maximo possivel para que ele sirva de referencia para
@@ -16,14 +16,18 @@
    Orientacao Prof: Luciana Akemi Iakabayashi (Tecnologias de Sistemas para Web)
 */
 
-/* Criamos algumas variaveis globais para tornar mais claro e rapido o codigo 
+/* 
+   Criamos algumas variaveis globais para tornar mais claro e rapido o codigo 
    seguinte. Os valores iniciais foram zerados (null), pois os valores definitivos
-   serao colocados quando a funcao 'init' for executada. */
+   serao colocados quando a funcao 'init' for executada. 
+*/
 var VISOR_DA_CALCULADORA = null,
     LINHA_DE_SINTAXE = null;
 
-/* A funcao 'init' e a primeira funcao que vamos rodar apos o download da pagina HTML
-   Ver evento 'ready' no final deste arquivo. */
+/* 
+   A funcao 'init' e a primeira funcao que vamos rodar apos o download da pagina HTML
+   Ver evento 'ready' no final deste arquivo. 
+*/
 function init() {
 	// Seta algumas variaveis globais
 	VISOR_DA_CALCULADORA = $("#entrada"); // obj do tipo input
@@ -33,15 +37,19 @@ function init() {
 	VISOR_DA_CALCULADORA.on("focus", function() { setSintaxe(""); });
 	$("#botoes_calc").on("mouseenter", function() { setSintaxe(""); });
 	
-	/* Aqui setamos o evento 'click' de *todos* os botoes da calculadora. Todos eles 
+	/* 
+	   Aqui setamos o evento 'click' de *todos* os botoes da calculadora. Todos eles 
 	   apontarao para uma funcao chamada 'processBotaoClique', ou seja, toda vez que 
-	   um botao for clicado	aquela funcao sera chamada para tratar o evento de clique. */
+	   um botao for clicado	aquela funcao sera chamada para tratar o evento de clique. 
+	*/
 	$("#botoes_calc button").each(function(i) {
 		// Este bloco sera executado para cada botao existente dentro da DIV 'botoes_calc'
 		var o = $(this); // 'o' contem agora o botao em si
-		// A funcao 'console' no javascript e' muito util para debugar o programa.
-		// Aqui estamos apenas dando um exemplo de uso. Para ver esta saida no Chrome, 
-		// use CTRL+SHIFT+I
+		/*
+		   A funcao 'console' no javascript e' muito util para debugar o programa.
+		   Aqui estamos apenas dando um exemplo de uso. Para ver esta saida no Chrome, 
+		   use CTRL+SHIFT+I
+		*/
 		console.log(i + " - Setando evento 'click' para o botao (" + o.attr("id") + ")");
 		// Definiremos agora a funcao que sera executada no evento 'click'
 		o.click(function(event) { 
@@ -67,12 +75,14 @@ function processBotaoClique(o) {
 // - - - Funcoes do Visor
 function setDadosVisor(s) { VISOR_DA_CALCULADORA.val( s );} // Coloca dados
 function getDadosVisor() { return VISOR_DA_CALCULADORA.val(); } // Recupera os dados
-function addDadosVisor(s) {	VISOR_DA_CALCULADORA.val( VISOR_DA_CALCULADORA.val() + s ); } // Adiciona dados
+function addDadosVisor(s) { VISOR_DA_CALCULADORA.val( VISOR_DA_CALCULADORA.val() + s ); } // Adiciona dados
 
 function setSintaxe(s) { LINHA_DE_SINTAXE.val(s); }
 
 // - - - 
 
-// Apos a carga de toda pagina HTML um evento (ready) e' enviado, e aqui 
-// estamos dizendo qual funcao rodar quando este evento ocorrer.
+/* 
+   Apos a carga da pagina HTML (na realidade do DOM) um evento (ready) e' 
+   enviado, e aqui estamos dizendo qual funcao rodar quando este evento ocorrer.
+*/
 $(document).ready( init );
